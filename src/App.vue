f4b460425f5d3bd3c875603ecac8248eca39b3ee
Edison <hydra1983@gmail.com>
2022-02-02 22:03:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-code-mirror
+      v-model:value="content"
+      lang="html"
+      theme="chrome"
+      style="height: 300px"/>
+</template>
+
+<script lang="ts">
+import CodeMirror from 'codemirror';
+import {VCodeMirror} from "vue3-code-mirror";
+import 'codemirror/mode/yaml/yaml';
+export default {
+  name: "app",
+  data: () => ({
+    content: "",
+    mode: "text/x-yaml"
+  }),
+  mounted: () => {
+    console.log(CodeMirror.modes);
+    console.log(CodeMirror.mimeModes);
+  },
+  components: {
+    VCodeMirror
+  }
+};
+</script>
